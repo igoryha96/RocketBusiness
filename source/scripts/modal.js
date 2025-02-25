a6235ec55224e93e518e0ec0fa25bdcbf88b8dd5
Игорь <90267795+igoryha96@users.noreply.github.com>
2025-02-25 23:22:09 +0300

    открывает и закрывает модалку

--- source/scripts/modal.js
+++ source/scripts/modal.js
@@ -1,8 +1,14 @@
+const modalWindow = document.querySelector('.modal');
+const content = document.querySelector('.content');
 const form = document.querySelector('.modal-form');
 
 const input = document.querySelector('.modal__name');
 const inputNumber = document.querySelector('.modal__phone');
 const errorMessage = document.querySelector('.modal__erorr');
+
+const closeModal = document.querySelector('.modal__close');
+const openButtons = document.querySelectorAll('.button');
+
 
 // input.addEventListener('invalid', (event) => {
 //   event.preventDefault();
@@ -21,4 +27,16 @@ input.addEventListener('keydown', () => {
     // Если не пустое, можно выполнить отправку формы или другие действия
     errorMessage.classList.add('modal__erorr--erorr');
   }
+});
+
+closeModal.addEventListener('click', () => {
+  modalWindow.classList.add('modal--close');
+  content.classList.remove('content-none');
+});
+
+openButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    content.classList.add('content-none');
+    modalWindow.classList.remove('modal--close');
+  });
 });
